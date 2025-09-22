5475c86b20ae683b95bbc396624c28e67f076e9e
Danny Stewart <danny@stewart.cc>
2025-09-22 08:48:54 -0400

    feat(volumehud): update to version 1.2.2
    Updates package definition to track latest upstream release with corresponding checksum verification for secure installation

--- Casks/volumehud.rb
+++ Casks/volumehud.rb
@@ -1,6 +1,6 @@
 cask "volumehud" do
-  version "1.2.0"
-  sha256 "147b9b591b542fdd7854448cfb77fab30bc30f2f7c0ee0bf7908550c1c0705c3"
+  version "1.2.2"
+  sha256 "b6fb2a8f5058eb2835c7c9d36449fdf214cd6ed55ac5cc6322454e57a2afa734"
 
   url "https://github.com/dannystewart/volumeHUD/releases/download/v#{version}/volumeHUD-#{version}.dmg"
   name "volumeHUD"
