baa99bc856f61080912bf80b40b052f0c69a342d
Danny Stewart <danny@stewart.cc>
2025-09-23 13:21:21 -0400

    feat(volumehud): updates to version 1.2.3
    Updates package definition with new release version and corresponding SHA256 checksum to provide users with latest features and bug fixes from upstream project.

--- Casks/volumehud.rb
+++ Casks/volumehud.rb
@@ -1,10 +1,10 @@
 cask "volumehud" do
-  version "1.2.2"
-  sha256 "b6fb2a8f5058eb2835c7c9d36449fdf214cd6ed55ac5cc6322454e57a2afa734"
+  version "1.2.3"
+  sha256 "9c98a7904bff9843c9948af50e5c6c21e07afe556a55e0bb0493c35dfd0c0fdb"
 
   url "https://github.com/dannystewart/volumeHUD/releases/download/v#{version}/volumeHUD-#{version}.dmg"
   name "volumeHUD"
-  desc "macOS volume HUD from pre-Tahoe"
+  desc "Restores the macOS volume HUD from pre-Tahoe"
   homepage "https://github.com/dannystewart/volumeHUD"
 
   livecheck do
