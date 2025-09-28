34f832c917ce21cd6365ea6da91ac0c8b307f2cf
Danny Stewart <danny@stewart.cc>
2025-09-28 09:26:03 -0400

    feat(volumehud): update to version 1.2.6
    Updates the Homebrew cask to pull the latest release with corresponding SHA256 hash verification for improved functionality and security patches.

--- Casks/volumehud.rb
+++ Casks/volumehud.rb
@@ -1,6 +1,6 @@
 cask "volumehud" do
-  version "1.2.5"
-  sha256 "f09ac41584267e10ef70880c2fe1cd131391d6d4916e872901a32fcadd958b98"
+  version "1.2.6"
+  sha256 "2805016224ebef4be65c3c4ab327003554d8f1b3c8ca85b60224d6cfd48c381e"
 
   url "https://github.com/dannystewart/volumeHUD/releases/download/v#{version}/volumeHUD-#{version}.dmg"
   name "volumeHUD"
