1cc2131bf08df55c92252a89a33aca239585aa65
Danny Stewart <danny@stewart.cc>
2025-09-28 08:00:54 -0400

    feat(volumehud): update to version 1.2.5
    Updates the Homebrew cask to pull the latest release with corresponding checksum verification to ensure users receive the most recent features and bug fixes.

--- Casks/volumehud.rb
+++ Casks/volumehud.rb
@@ -1,6 +1,6 @@
 cask "volumehud" do
-  version "1.2.4"
-  sha256 "dffe9d1cd1ed30e55dab6a87f7ddb71af1ff4c006b74659a5c49bd6c844c588c"
+  version "1.2.5"
+  sha256 "f09ac41584267e10ef70880c2fe1cd131391d6d4916e872901a32fcadd958b98"
 
   url "https://github.com/dannystewart/volumeHUD/releases/download/v#{version}/volumeHUD-#{version}.dmg"
   name "volumeHUD"
