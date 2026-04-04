9a5290879098f1687f41bf5b323649df893bd71d
Danny Stewart <danny@stewart.cc>
2026-04-04 08:42:58 -0400

    style(ruby): convert string delimiters from double to single quotes

--- Casks/iplooker.rb
+++ Casks/iplooker.rb
@@ -3,18 +3,18 @@ cask "iplooker" do
   sha256 "53c49dba67b2584a00feff9bb698c5dfd2c645c1844c45255df2c9e699ab0816"
 
   url "https://github.com/dannystewart/IPLookerApp/releases/download/v#{version}/IPLooker-#{version}.dmg"
-  name "IPLooker"
-  desc "IP lookup app with multi-source aggregation"
-  homepage "https://github.com/dannystewart/IPLookerApp"
+  name 'IPLooker'
+  desc 'IP lookup app with multi-source aggregation'
+  homepage 'https://github.com/dannystewart/IPLookerApp'
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  depends_on macos: ">= :tahoe"
+  depends_on macos: '>= :tahoe'
 
-  app "IPLooker.app"
+  app 'IPLooker.app'
 
-  uninstall quit:       "com.dannystewart.IPLooker"
+  uninstall quit: 'com.dannystewart.IPLooker'
 end
--- Casks/volumehud.rb
+++ Casks/volumehud.rb
@@ -1,31 +1,31 @@
-cask "volumehud" do
-  version "3.3.1"
-  sha256 "e6a4189699c5cdf782939d591c0e7593611408256c7efa8370f4a16d99e7c038"
+cask 'volumehud' do
+  version '3.3.1'
+  sha256 'e6a4189699c5cdf782939d591c0e7593611408256c7efa8370f4a16d99e7c038'
 
   url "https://github.com/dannystewart/volumeHUD/releases/download/v#{version}/volumeHUD-#{version}.dmg"
-  name "volumeHUD"
-  desc " Bring back the pre-Tahoe macOS volume and brightness HUDs "
-  homepage "https://github.com/dannystewart/volumeHUD"
+  name 'volumeHUD'
+  desc ' Bring back the pre-Tahoe macOS volume and brightness HUDs '
+  homepage 'https://github.com/dannystewart/volumeHUD'
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  depends_on macos: ">= :tahoe"
+  depends_on macos: '>= :tahoe'
 
-  app "volumeHUD.app"
+  app 'volumeHUD.app'
 
-  uninstall quit:       "com.dannystewart.volumehud",
-            login_item: "volumeHUD"
+  uninstall quit: 'com.dannystewart.volumehud',
+            login_item: 'volumeHUD'
 
   zap trash: [
-    "~/Library/Application Support/volumeHUD",
-    "~/Library/Caches/com.dannystewart.volumehud",
-    "~/Library/Logs/volumeHUD",
-    "~/Library/Preferences/com.dannystewart.volumehud.plist",
-    "~/Library/Saved Application State/com.dannystewart.volumehud.savedState",
+    '~/Library/Application Support/volumeHUD',
+    '~/Library/Caches/com.dannystewart.volumehud',
+    '~/Library/Logs/volumeHUD',
+    '~/Library/Preferences/com.dannystewart.volumehud.plist',
+    '~/Library/Saved Application State/com.dannystewart.volumehud.savedState',
   ]
 
-  conflicts_with cask: "dannystewart/apps/volumehud-beta"
+  conflicts_with cask: 'dannystewart/apps/volumehud-beta'
 end
