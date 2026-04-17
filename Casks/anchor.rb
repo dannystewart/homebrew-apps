76d2153a9b090b6d1e2db4c6ac4275ab932e501e
Danny Stewart <danny@stewart.cc>
2026-04-17 14:10:29 -0400

    feat(anchor): add Anchor cask for ADHD-friendly task focus app

---
+++ Casks/anchor.rb
@@ -0,0 +1,20 @@
+cask 'anchor' do
+  version '1.0.0'
+  sha256 '35623e599e7583f1cbd9706d886dcb02c7d4dae16c7ea1c0b84b0f4c9fdfd450'
+
+  url "https://github.com/dannystewart/Anchor/releases/download/v#{version}/Anchor-#{version}.dmg"
+  name 'Anchor'
+  desc 'ADHD-friendly Mac app to help anchor you to your current task'
+  homepage 'https://github.com/dannystewart/Anchor'
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: '>= :tahoe'
+
+  app 'Anchor.app'
+
+  uninstall quit: 'com.dannystewart.Anchor'
+end
